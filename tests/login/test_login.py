9b68370f6c9c14217d56772eb9927ed427ef7d12
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-12-28 18:21:00 +0200

    Add login test

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,2 +1,39 @@
-def test_user_login(user_login_fixture):
-    assert True
+import pytest
+
+
+@pytest.mark.parametrize(
+    "username,password,expected_result",
+    [
+        pytest.param(
+            "qa.ajax.app.automation@gmail.com",
+            "qa_automation_password",
+            True,
+            id="Correct username and password",
+        ),
+        pytest.param(
+            "invalid_username",
+            "qa_automation_password",
+            False,
+            id="Incorrect username",
+        ),
+        pytest.param(
+            "qa.ajax.app.automation@gmail.com",
+            "invalid_password",
+            False,
+            id="Incorrect password",
+        ),
+        pytest.param(
+            "",
+            "",
+            False,
+            id="Empty fields",
+        ),
+    ],
+)
+def test_user_login(username, password, expected_result, user_login_fixture):
+    lp = user_login_fixture
+    result = lp.login(
+        username=username,
+        password=password
+    )
+    assert result is expected_result
